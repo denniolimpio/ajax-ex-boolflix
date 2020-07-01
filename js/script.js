a56c0655d3116229e9ea77ba62852ea261b5bde9
dennis <dennis.olimpio0794@gmail.com>
2020-07-01 13:22:37 +0200

    ho stampato la scheda del film

--- js/script.js
+++ js/script.js
@@ -3,6 +3,19 @@ $(document).ready( function() {
   // chiave api
   // 03a82650cf8ff5326e52310b09aeb357
 
+
+  //  cliccando sulla input leggo il valore inserito dall'utente
+
+  $("#search").click ( function () {
+
+  // valore inserito dall'utente
+
+  var inputSearch = $(this).val();
+
+  console.log(inputSearch);
+
+  });
+
 // chiamo l'api contenente le info sui film
   $.ajax (
 
@@ -13,7 +26,7 @@ $(document).ready( function() {
       {
 
       api_key: "03a82650cf8ff5326e52310b09aeb357",
-      query: 'mad max',
+      query: ' ritorno al futuro',
       language: 'it-IT',
 
     },
@@ -21,7 +34,55 @@ $(document).ready( function() {
 
     success: function(dataResponse) {
 
-      console.log(dataResponse);
+      var dataResults = dataResponse.results;
+
+      console.log(dataResults);
+
+      // ciclo for dataResults
+
+
+      for ( var i = 0; i < dataResults.length; i++) {
+
+        var arrayDataResutls = dataResults[i];
+        // console.log(arrayDataResutls);
+
+        // SCHEDA FILM
+        // devo mostrare
+        // -- >Titolo
+        // -- > Titolo Originale
+        // -- > Lingua
+        // -- > voto
+
+          var schedaFilm = ( {
+
+          title: arrayDataResutls.title,
+          original_title: arrayDataResutls.original_title,
+          original_language: arrayDataResutls.original_language,
+          vote_average: arrayDataResutls.vote_average,
+
+
+        });
+
+        console.log(schedaFilm);
+
+
+
+      };
+      // fine ciclo for dataResults
+
+
+      // stampo la scheda film con handlebars
+
+      var source = $("#movie-template").html();
+      var template = Handlebars.compile(source);
+
+      var html = template(schedaFilm);
+      $("#container").append(html);
+
+
+
+
+      // console.log(schedaFilm);
 
 
     },
@@ -32,20 +93,14 @@ $(document).ready( function() {
 
     }
 
+
 });
 
 
 
-// ------ funzioni
-
-// -- creo una funzione per leggere il valore inserito dall'utente nella searchBar
-// --> restituisco il titolo del film corrispondente al valore inserito dall'utente
-
-// function searchFilm() {
 
 
 
 
-// };
 
 }); // chiudo document ready
