b76fcf5d4b54959629b2f97aa34fa58d93e8d268
dennis <dennis.olimpio0794@gmail.com>
2020-07-02 15:29:59 +0200

    ho aggiunto alcuni commenti. Creo la funzione per aggiungere le bandiere

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
+// chiave api
+// 03a82650cf8ff5326e52310b09aeb357
+
 $(document).ready( function() {
-
-  // chiave api
-  // 03a82650cf8ff5326e52310b09aeb357
-
-
   //  cliccando su buttonsearch avvio la ricerca
   //--- > leggo il valore inserito dall'utente nella inputSearch
   // --> se la input non è vuota, invoca la funzione searchFilm e avvio la ricerca
   // -- > la funzione Searchfilm stampa  i risultati a schermo
 
   $("#buttonSearch").click ( function () {
-
     // invoco la funzione reset(), in questo modo svuoto il mio template
     reset();
     // con la funzione readInput() leggo il valore della input inserita dall'utente
@@ -18,25 +15,16 @@ $(document).ready( function() {
     avvioRicerca ()
   });
 
-
-
   // digitando il tasto invio (13)  avvio la ricerca
-
   $("#input").keypress ( function () {
-
     reset();
-
     if (event.which === 13 || event.keyCode === 13) {
-
       avvioRicerca ()
-
     }
   });
-
-
   //  ------- Funzioni -------
 
-
+  //  #libreriaFilm -------
 
   function libreriaFilm (film) {
 
@@ -46,40 +34,31 @@ $(document).ready( function() {
     // chiamo l'api contenente le info sui film
 
     $.ajax (
-
       {
         url: urlFilm,
         method: 'GET',
         data:
         {
-
           api_key: myKey,
           query: film,
           language: 'it-IT',
-
         },
         // se l'api viene caricata correttamente
-
         success: function(dataResponse) {
 
           var dataResults = dataResponse.results;
-
           // console.log(dataResults);
-
           stampaRisultato(dataResults)
         },
+        // altrimenti se l'api non viene caricata...errore
 
         error: function() {
-
           alert( "attenzione, si è verificato un erroe")
-
         }
-
-
       });
     }
 
-
+    //  #libreriaTv -------
 
     function libreriaTv (tv) {
 
@@ -87,7 +66,6 @@ $(document).ready( function() {
       var myKey = "03a82650cf8ff5326e52310b09aeb357";
 
       // chiamo l'api contenente le info sui film
-
       $.ajax (
 
         {
@@ -95,7 +73,6 @@ $(document).ready( function() {
           method: 'GET',
           data:
           {
-
             api_key: myKey,
             query: tv,
             language: 'it-IT',
@@ -113,99 +90,87 @@ $(document).ready( function() {
           },
 
           error: function() {
-
             alert( "attenzione, si è verificato un erroe")
-
           }
-
-
         });
       }
 
-    //  Con questa funzione compilo il  template
-    function stampaRisultato(dataResults) {
+      //  #stampaRisultato -------
+
+      // compilo il template con handlebars
+      function stampaRisultato(dataResults) {
+        var source = $("#template").html();
+        var template = Handlebars.compile(source);
+
+        for ( var i = 0; i < dataResults.length; i++) {
+
+          var arrayDataResutls = dataResults[i];
+          // console.log(arrayDataResutls);
 
 
-      var source = $("#template").html();
-      var template = Handlebars.compile(source);
+          // dichiaro le variabili
 
+          // titolo serie tv
+          var titoloSerie = arrayDataResutls.name;
+          var voto = arrayDataResutls.vote_average;
+          //converto i numeri da decimali a interi
+          var star  =convertoVoto(voto);
 
-      for ( var i = 0; i < dataResults.length; i++) {
+          // SCHEDA FILM
+          var scheda = ( {
+            title: arrayDataResutls.title,
+            name: titoloSerie,
+            original_title: arrayDataResutls.original_title,
+            original_language: arrayDataResutls.original_language,
+            overview: arrayDataResutls.overview,
+            vote_average: star,
+          });
 
-        var arrayDataResutls = dataResults[i];
-        // console.log(arrayDataResutls);
+          var html = template(scheda);
+          $("#container").append(html);
+        }
 
-        // SCHEDA FILM
-        // devo mostrare
-        // -- >Titolo
-        // -- > Titolo Originale
-        // -- > Lingua
-        // -- > voto
-
-
-
-        // dichiaro le variabili
-        var titoloSerie = arrayDataResutls.name;
-        var voto = arrayDataResutls.vote_average;
-        var star  =convertoVoto(voto);
-
-
-
-        var scheda = ( {
-
-
-
-          title: arrayDataResutls.title,
-          name: titoloSerie,
-          original_title: arrayDataResutls.original_title,
-          original_language: arrayDataResutls.original_language,
-          overview: arrayDataResutls.overview,
-          vote_average: star,
-
-        });
-        var html = template(scheda);
-        $("#container").append(html);
       }
 
-    }
+      // ---- #reset
 
+      //---> elimino il contenuto del template
+      function reset() {
+        $("#container").html("");
+      }
 
-    // funzione reset
+      // ---- #avvioRicerca
+      //  -- > se valore input e, se qnon è vuoto, stampo a schermo i risultati della ricerca
 
-    function reset() {
-      $("#container").html("");
-    }
+      function avvioRicerca () {
 
+        var inputSearch = $("#input").val();
 
-// leggo valore input e, se qnon è vuoto, stampo a schermo i risultati della ricerca
+        if ( inputSearch != "") {
+          libreriaFilm(inputSearch);
+          libreriaTv (inputSearch);
+        }
 
-  function avvioRicerca () {
+      }
+      // --- >  #convertoVoto
 
-    var inputSearch = $("#input").val();
+      // creo una funzione per convertire il voto da decimale a intero ( 1 - 5 )
+      // per voti compresi tra 1 e 5 aggiugno le stelle piene
+      // ---> altrimenti aggiungo le stelle vuote
 
-    if ( inputSearch != "") {
-      libreriaFilm(inputSearch);
-      libreriaTv (inputSearch);
-    }
+      function convertoVoto(vote_av) {
+        var vote = Math.floor(vote_av / 2);
+        var stars = '';
+        for (var i = 1; i <= 5; i++) {
+          if (i <= vote) {
+            stars += '<i class="fas fa-star"></i>';
+          } else {
+            stars += '<i class="far fa-star"></i>';
+          }
+        }
+        return stars;
+      }
 
-  }
+      // --- > #bandiere
 
-
-// creo una funzione per convertire il voto da decimale a intero ( 1 - 5 )
-// per voti compresi tra 1 e 5 aggiugno le stelle piene
-// ---> altrimenti aggiungo le stelle vuote
-function convertoVoto(vote_av) {
-   var vote = Math.floor(vote_av / 2);
-   var stars = '';
-   for (var i = 1; i <= 5; i++) {
-       if (i <= vote) {
-         stars += '<i class="fas fa-star"></i>';
-       } else {
-         stars += '<i class="far fa-star"></i>';
-       }
-   }
-   return stars;
- }
-
-
-  }); // chiudo document ready
+    }); // chiudo document ready
